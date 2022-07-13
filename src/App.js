a7ad6eccfa8782edcd47febd1cdf9e15622d8f9a
David <Rigby335@hotmail.com>
2022-07-12 17:46:23 -0700

    Link / Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import HomePage from './HomePage';
+import Movie from './Movie';
+import SearchPage from './SearchPage';
+import WatchListPage from './WatchListPage';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/watchlist">Watch List</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/watchlist">
+            <WatchListPage />
+          </Route>
+          <Route exact path="/search">
+            <SearchPage />
+          </Route>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
